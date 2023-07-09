48683389868d59636610fb8ceb4c00ed792966a4
preethy12 <preethyray12@.com>
2023-07-09 22:30:53 +0530

    slider

---
+++ slider.js
@@ -0,0 +1,51 @@
+
+// $('.slider').slick({
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 1500,
+// });
+// $('.customer-logos').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
+// $('.slider').slick({
+//   dots: true,
+//   infinite: true,
+//   autoplay: true,
+//   autoplaySpeed: 6000,
+//   speed: 800,
+//   slidesToShow: 2,
+//   adaptiveHeight: true
+// });
+
+$('.quotes').slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+
+//   $('.slick-slider').slick({
+//       centerMode: true,
+//       centerPadding: '60px',
+//       slidesToShow: 3,
+//       prevArrow: $('.prev'),
+//       nextArrow: $('.next'),
+// });
+$(function() {
+    $('.slider__container-2').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 5
+    });
+    $('.slider__container').slick({
+      fade : true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+  });
+});
+
+
